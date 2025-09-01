ae638eb05b86aad1ade27a9ca823ed582231d978
shivshankar8ss <shivshankar8w@gmail.com>
2025-09-02 01:13:48 +0530

    fixing issues

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://airbnb-ss.onrender.com",
     credentials: true,
   })
 );
