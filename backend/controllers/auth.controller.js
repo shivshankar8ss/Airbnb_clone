8641129c6c8621aff1136b55b43518408c4c7ef5
shivshankar8ss <shivshankar8w@gmail.com>
2025-09-02 01:07:08 +0530

    fixing issues

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -28,8 +28,8 @@ export const signUp = async (req, res) => {
     const token = await genToken(user._id);
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: true,
+      sameSite:"none" ,
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -73,8 +73,8 @@ export const logOut = async (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: true,
+      sameSite: "none",
     });
 
     return res.status(200).json({ message: "Logout successfully" });
